1b86160552e7b9703ace5c719eb69682b96c7ab4
SlipStreme <Kinloes@gmail.com>
2023-08-11 18:37:58 -0400

    Added listeners to buttons Also tested that they all function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,53 @@
-console.log("test")
+//Button event listeners for numbers
+document.getElementById("zero").onclick = function(){
+    console.log("0")
+}
+document.getElementById("one").onclick = function(){
+    console.log("1")
+}
+document.getElementById("two").onclick = function(){
+    console.log("2")
+}
+document.getElementById("three").onclick = function(){
+    console.log("3")
+}
+document.getElementById("four").onclick = function(){
+    console.log("4")
+}
+document.getElementById("five").onclick = function(){
+    console.log("5")
+}
+document.getElementById("six").onclick = function(){
+    console.log("6")
+}
+document.getElementById("seven").onclick = function(){
+    console.log("7")
+}
+document.getElementById("eight").onclick = function(){
+    console.log("8")
+}
+document.getElementById("nine").onclick = function(){
+    console.log("9")
+}
+//Button event listeners for commands
+document.getElementById("equal").onclick = function(){
+    console.log("=")
+}
+document.getElementById("clear").onclick = function(){
+    console.log("clear")
+}
+document.getElementById("delete").onclick = function(){
+    console.log("delete")
+}
+document.getElementById("add").onclick = function(){
+    console.log("+")
+}
+document.getElementById("minus").onclick = function(){
+    console.log("-")
+}
+document.getElementById("multiply").onclick = function(){
+    console.log("*")
+}
+document.getElementById("divide").onclick = function(){
+    console.log("/")
+}
